624017279aca523329efc84c6e9bbbbe513da0ea
Samueltommzy <samueloluwatommzy@gmail.com>
2021-03-21 22:55:09 +0000

    Implemented User abstract class methods in patient class

---
+++ src/model/Patient.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author samuel
+ */
+public class Patient extends User{
+     private int id;
+
+    private String fullName;
+
+    private String address;
+
+    private String phoneNumber;
+    
+    public Patient(int id,String fullName,String address,String phoneNumber){
+         this.id = id;
+        this.fullName = fullName;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public int getId() {
+       return id;
+    }
+
+    @Override
+    public String getFullName() {
+        return fullName;
+    }
+
+    @Override
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public String getUserInfo() {
+        return "Id: " + getId() + "\nName: " + getFullName() + "\nAddress: " + getAddress() + "\nphoneNumber: " +getPhoneNumber();
+    }  
+}
